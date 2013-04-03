4c1b0129ed0fc6fdb38a33118a387824e3add47a
AlexandreBeaulne <alexandre.beaulne@gmail.com>
2013-04-03 16:18:56 -0400

    finished the DEV mode

--- processor.py
+++ processor.py
@@ -2,28 +2,36 @@ import pycurl
 import json
 import threading
 import Queue
+import pickle
 
 class Processor(threading.Thread):
 
-    def __init__(self, username, password, tweets):
+    def __init__(self, username, password, tweets_queue, dev_mode):
         threading.Thread.__init__(self)
-        self.tweets = tweets
-        self.stream_url = 'https://stream.twitter.com/1.1/statuses/sample.json'
-        self.username = username
-        self.password = password
-        self.attributes = ['text', 'id', 'coordinates', 'created_at']
-        self.buffer = ""
-        self.conn = pycurl.Curl()
-        self.conn.setopt(pycurl.USERPWD, "%s:%s" % (self.username, self.password))
-        self.conn.setopt(pycurl.URL, self.stream_url)
-        self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
+        self.tweets_queue = tweets_queue
+        self.dev_mode = dev_mode
+        if not self.dev_mode:
+            self.stream_url = 'https://stream.twitter.com/1.1/statuses/sample.json'
+            self.username = username
+            self.password = password
+            self.attributes = ['text', 'id', 'coordinates', 'created_at']
+            self.buffer = ""
+            self.conn = pycurl.Curl()
+            self.conn.setopt(pycurl.USERPWD, "%s:%s" % (self.username, self.password))
+            self.conn.setopt(pycurl.URL, self.stream_url)
+            self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
 
     def run(self):
-        print('running...')
-        self.conn.perform()
+        if not self.dev_mode:
+            self.conn.perform()
+        else:
+            print("\n%%%%% Executing in DEV mode %%%%%\n")
+            tweets = pickle.load(open("tweets.p","rb"))
+            for tweet in tweets:
+                self.tweets_queue.put(tweet)
+                print(json.dumps(tweet, indent=4, separators=(',', ': ')))
 
     def on_receive(self, data):
-        print('received message')
         self.buffer += data
         #if data.endswith("rn") and self.buffer.strip():
         content = json.loads(self.buffer)
@@ -31,6 +39,6 @@ class Processor(threading.Thread):
 
         if 'text' in content.keys():
             res = {key:value for (key,value) in content.items() if key in self.attributes}
-            self.tweets.put(res)
+            self.tweets_queue.put(res)
             print(json.dumps(res, indent=4, separators=(',', ': ')))
 
--- supervisor.py
+++ supervisor.py
@@ -7,22 +7,39 @@ Spawn the twitter stream producer thread
 Set up queue
 Spawn consumer thread
 
+Usage:
+    $ python supervisor.py uSErName pa$$w0rd
+
+    or
+
+    $ python supervisor.py -D uSErName pa$$w0rd # for turning dev mode on
+
+
 Development Twitter account is:
 Username: ornitweet
 Password: ornithology
 """
 
 class Supervisor(object):
-    def __init__(self, username, password):
+    def __init__(self, username, password, dev_mode):
         self.username = username
         self.password = password
-        self.tweets = Queue.Queue()
+        self.tweets_queue = Queue.Queue()
+        self.dev_mode = dev_mode
 
     def launch(self):  
-        p = processor.Processor(self.username, self.password, self.tweets)
+        p = processor.Processor(self.username, self.password, self.tweets_queue, self.dev_mode)
         p.start()
 
 if __name__=="__main__":
-    _, username, password = sys.argv
-    Supervisor(username, password).launch()
+    if len(sys.argv) == 3:
+        _, username, password = sys.argv
+        dev_mode = False
+    elif len(sys.argv) == 4 and sys.argv[1] in ["-d","-D"]:
+        _, dev_mode, username, password = sys.argv
+        dev_mode = True
+    else:
+        print(supervisor.__doc__)
+
+    Supervisor(username, password, dev_mode).launch()
 
