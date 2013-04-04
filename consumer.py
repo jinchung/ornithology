d1837b54dc3753ff862cb0c92950e47c4933d394
jinchung <jin.chung17@gmail.com>
2013-04-04 11:21:43 -0400

    added logfile for consumer logs

--- consumer.py
+++ consumer.py
@@ -14,14 +14,15 @@ class Consumer(threading.Thread):
         self.tweets_queue = tweets_queue
         self.keywords = keywords
 
+        self.logfile = open('matched_tweets.log', 'w')
+        
     def run(self):
-
-        print "Keyword".rjust(COL1), "Time".rjust(COL2), "Place".rjust(COL3)              
-        print "===========================================================================" 
         while True:
             tweet = self.tweets_queue.get(True)
             text = set(tweet['text'].lower().split())
             matches = self.keywords.intersection(text)
             for match in matches:
                 print match.rjust(COL1), tweet['created_at'].rjust(COL2), str(tweet["coordinates"]).rjust(COL3)
+                self.logfile.write(match.rjust(COL1) + tweet['created_at'].rjust(COL2) + str(tweet["coordinates"]).rjust(COL3) + '\n')
+                self.logfile.flush()
 
